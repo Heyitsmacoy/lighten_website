298e4150cc38b483586bcc76652398ffb70f77e7
REN <reniel262@gmail.com>
2020-11-19 15:18:00 +0800

    auth&fpass

--- routes/user-route.js
+++ routes/user-route.js
@@ -1,52 +1,31 @@
 const router = require('express').Router()
-const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs') 
 const jwt = require('jsonwebtoken')
-var request = require('request')
+const mailgun = require("mailgun-js")
+const DOMAIN = "sandbox21a46e953d154129a601ac7b91d76885.mailgun.org"
+const mg = mailgun({apiKey: process.env.MAILGUN_APIKEY, domain: DOMAIN})
 
+//import authentication
+const auth = require('../middleware/auth')
 // import user-model
 const User = require('../models/user-model')
-const { json } = require('body-parser')
-//import middleware
-const auth = require('../middleware/auth')
-const mail = require('../middleware/mailing')
+
 
 // ROUTES
-// Home route
-router.get('/', (req, res) => {
-    res.render("index.ejs")
-})
-// Registration route
-router.get('/register', (req, res) => {
-    res.render("register.ejs")
-})
-// terms&privacy route
-router.get('/term&policy', (req, res) => {
-    res.render("terms.ejs")
-})
-// forgotpass route
-router.get('/forgotPassword', (req, res) => {
-    res.render("fpw-confirm.ejs")
-})
-
-router.post('/emailfpw', mail.forgot_pass)
-
 router.post('/register', async(req, res) => {
-    var username = req.body.uName;
-    var password = req.body.password;
-    var cPassword = req.body.cPassword;
-    var email = req.body.email;
-
     // VALIDATIONS
     try{
+        const {email, password, passwordCheck, displayName} =req.body
         // check if passwords match
-        if(password !== cPassword){
+        if(password !== passwordCheck){
+            console.log("pass")
             return res
                 .status(400)
                 .json({ msg: "Passwords do not match" })
         }
         // check if there as in exisiting username
-        const existingUser = await User.findOne({ username: username })
+        const existingUser = await User.findOne({ username: displayName })
+        console.log("uname")
             if(existingUser){
                 return res
                     .status(400)
@@ -56,6 +35,7 @@ router.post('/register', async(req, res) => {
                 //check if there is an existing email
                 const exisitingEmail = await User.findOne({ email: email })
                 if(exisitingEmail){
+                    console.log("em")
                     return res
                         .status(400)
                         .json({ msg: "Email already exists"})
@@ -64,15 +44,14 @@ router.post('/register', async(req, res) => {
                     //hashing passowrd
                     const salt = await bcrypt.genSalt(10)
                     const hashedPassword = await bcrypt.hash(password, salt)
-
                     //saving data to database
                     const newUser = new User({
-                        username,
+                        username: displayName,
                         password: hashedPassword,
-                        email
+                        email: email
                     })
-                    newUser.save()
-                    res.json(newUser) 
+                    const savedUser = await newUser.save()
+                    res.json(savedUser) 
                 }
             }
     }catch(err){
@@ -81,36 +60,39 @@ router.post('/register', async(req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-    var unlog = req.body.unlog
-    var pwlog = req.body.pwlog
-
     // VALIDATIONS
     try{
+        const {email, password, displayName} = req.body
         // check for username or email
-        const user = await User.findOne({$or: [{username:unlog}, {email:unlog}]})
+        const user = await User.findOne({$or: [{username:displayName}, {email:email}]})
         if(user){
             // comparing of password and hashed password
-            bcrypt.compare(pwlog,user.password, (err, isMatch) => {
+            bcrypt.compare(password,user.password, (err, isMatch) => {
                 if(isMatch){
                     // check designations
                     if(user.designation === "admin"){
-                        res.json({msg: "logged in as an admin"})
+                        const token = jwt.sign({id: user._id}, process.env.JWT_TOKEN)
+                        res.json({
+                            token,
+                            user:{
+                                id: user._id,
+                                name: user.username,
+                                email: user.email,
+                            },
+                        })
+
                     }
                     else{
-                        const token = jwt.sign({ id: user.username}, process.env.JWT_TOKEN)
-                        var url = process.env.CLIENT_URL + "/login/"+token
-                        // console.log(url)
-                        // console.log(token)
-                        //request(url, function(error,response,body){
-                          //  res.status(200).send(body)
-                            res.send({
-                                token,
-                                user:{
-                                    name: user.username,
-                                    email: user.email
-                                },
-                            })
-                          //})
+                        const token = jwt.sign({id: user._id}, process.env.JWT_TOKEN)
+                        console.log(token)
+                        res.json({
+                            token,
+                            user:{
+                                id: user._id,
+                                name: user.username,
+                                email: user.email,
+                            },
+                        })
                     }
                 }
                 else{
@@ -132,33 +114,77 @@ router.post('/login', async (req, res) => {
     
 })
 
-
-// Login Route
-router.get('/login/:token', (req, res) => {
-    const token = req.params.token
-    console.log(token)
-    console.log("geh")
-    res.render("admin.ejs")
+router.post('/forgotpass', async (req, res) => {
+    try{
+        function getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min) + min); 
+        }
+        const veri_code = getRandomInt(100000, 999999)
+        const email = req.body.email
+        console.log(email)
+        const user = await User.findOne({email:email})
+        if(!user){
+            return res
+                .status(400)
+              .json({ msg: "User doesn't exist" })
+        }else{
+            const data = {
+                from: 'elighten2020@gmail.com',
+                to: email,
+                subject: 'Account Activate ',
+                html: `
+                    <h2> Verification Code </h2>
+                    <p>${veri_code}</p>
+                    
+                `
+            }
+            res.json({
+                user:{
+                    id: user._id,
+                    name: user.username,
+                    email: user.email,
+                    code: veri_code,
+                },
+            })
+        }
+    }
+    catch(err){
+        res.status(500).json({error:err.message})
+    }
 })
 
-router.post('/token_validation',auth, async(req,res) =>{
-    try {
+router.post('/tokenIsValid', async (req, res) => {
+    try{
         const token = req.header("x-auth-token")
-        console.log(token)
-        if(!token){
+        if(!token){ 
             return res.json(false)
-        }else{
-            const verified = jwt.verify(token, process.env.JWT_TOKEN)
-            if(!verified){
-                return res.json(false)
-            }else{
-                const user = await User.findOne(verified.id)
-                console.log(user)
-                console.log(verified)
-            }
         }
-    } catch (err) {
-        res.status(500).json({error: err.message})
+
+        const verified = jwt.verify(token, process.enc.JWT_TOKEN)
+        if(!verified){ 
+            return res.json(false)
+        }
+
+        const user = await User.findOne(verified.id)
+        if(!user){ 
+            return res.json(false)
+        }
+        return res.json(true)
+
+    } catch(err){
+        res.status(500).json({ error: err.message })
     }
- })
+
+})
+
+router.get("/", auth, async(req, res) => {
+    const user = await User.findById(req.user)
+    res.json({
+        displayName: user.username,
+        id: user._id,
+    })
+})
+
 module.exports = router
